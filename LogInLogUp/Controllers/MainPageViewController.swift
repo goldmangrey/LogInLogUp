87bfbfe60e30d54b7dff17498774fe582c6985c8
Yeskendir <yeskendiriskakov@mail.ru>
2023-05-25 23:21:24 +0600

    Creating Design for login page

--- LogInLogUp/Controllers/MainPageViewController.swift
+++ LogInLogUp/Controllers/MainPageViewController.swift
@@ -11,9 +11,21 @@ class MainPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "Logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true)
+        }
+        
+    }
+    
 }
 

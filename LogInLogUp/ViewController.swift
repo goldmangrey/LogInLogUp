63be5231ec2e14631b1a130c53d8df7ad3b7a764
Yeskendir <yeskendiriskakov@mail.ru>
2023-05-25 18:18:37 +0600

    Initial Commit

---
+++ LogInLogUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInLogUp
+//
+//  Created by Yeskendir on 25.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
